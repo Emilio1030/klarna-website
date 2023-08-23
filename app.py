f823d72bab5a1d33bffdc1c69b6d8246a9cdac0d
Emilio Aguiar <junioraguiar_83@hotmail.com>
2023-08-23 17:12:26 +0100

    My version 28

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ from PIL import Image
 
 # GitHub repository [Klarna](https://github.com/Emilio1030/klarna)
 # '''
+# SETTING PAGE IMAGE & NAME
 im = Image.open("image/klarna_icon_1.png")
 st.set_page_config(
     page_title="Klarna Project",
@@ -36,6 +37,7 @@ lottie_coding = load_lottieurl("https://lottie.host/cfb13562-6706-40b8-9a0a-aeb3
 with open('style.css') as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
+# ICON
 github_icon_svg = '''
 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512" style="width: 1.28em; height: 2em; color: white;">
 <path d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"/></svg>
@@ -55,6 +57,15 @@ klarna_image = '''
 github_link = 'https://github.com/Emilio1030'
 linkedin_link = 'https://www.linkedin.com/in/emilioaguiar/'
 email_link = 'mailto:junioraguiar_83@hotmail.com'
+#############
+# GIPHY IMAGE
+giphy_url = "https://media.giphy.com/media/N013zOWpvY7O9T2XYP/giphy.gif"
+html_code = f"""
+<a href="{giphy_url}" target="_blank">
+    <img src="{giphy_url}" width="300" alt="Giphy GIF" />
+</a>
+"""
+###############
 # Load external CSS file
 st.markdown('<link rel="stylesheet" href="custom.css">', unsafe_allow_html=True)
 # Streamlit layout structure
@@ -91,9 +102,13 @@ with st.container():
 
                 st.write("Prediction Result:")
                 st.write(f"Customer ID: {prediction_data['customer_ID']}")
-                st.write(f"Output: {prediction_data['output']}")
                 st.write(f"Probability: {prediction_data['probability']}")
-
+                if prediction_data['probability'] <= 0.20:
+                   # st.write("![Your Awsome GIF](https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExdWFwbHpvaWh2N3p1MWxydzBpNjdneXNzMWRqYnB5OXRvMTZqeDYxdSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/N013zOWpvY7O9T2XYP/giphy.gif)")
+                    st.write(html_code, unsafe_allow_html=True)
+                else:
+                    st.write("![Not approved](https://media.giphy.com/media/9B5bcqtSnJq44vtsvn/giphy.gif")
+                # st.write(f"Output: {prediction_data['output']}")
             else:
                 st.error(f"Error: {response.status_code}")
 
